68d4d57db4de48aa0810fa13f155da6acb8d9f46
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2025-01-26 16:01:01 +0330

    Add files via upload

---
+++ BlockVpn.php
@@ -0,0 +1,26 @@
+<?php
+$url = "https://api.ipapi.is";
+
+$one = curl_init($url);
+curl_setopt($one, CURLOPT_RETURNTRANSFER, true);
+$tow = curl_exec($one);
+curl_close($one);
+$data = json_decode($tow, true);
+
+$ip = $data['ip'] ?? 'N/A';
+$country = $data['location']['country'] ?? 'N/A';
+$company_domain = $data['company']['domain'] ?? 'N/A';
+$local_time = $data['location']['local_time'] ?? 'N/A';
+$proxy = $data['is_proxy'] ?? 'N/A';
+$vpn = $data['is_vpn'] ?? 'N/A';
+
+$response = [
+    'ip' => $ip,
+    'country' => $country,
+    'company_domain' => $company_domain,
+    'local_time' => $local_time,
+    'server' => (($vpn || $proxy === 1) ? '<p style="color:red;">Dsconnection</p>' : '<p style="color:green;">Connection</p>')
+];
+
+echo json_encode($response);
+?>
